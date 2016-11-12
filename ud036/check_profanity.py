d4ee485172f8885bf550b3397382432f671bf908
Victor Tamayo <victor.t.barriga@gmail.com>
2016-11-11 23:23:03 -0800

    profanity checker

--- ud036/check_profanity.py
+++ ud036/check_profanity.py
@@ -1,5 +1,19 @@
+import urllib
+
 def read_text():
     quotes = open("movie_quotes.txt")
     contents_of_file = quotes.read()
+    check_profanity(contents_of_file)
+    quotes.close()
+
+def check_profanity(text_to_check):
+    url = "http://www.wdylike.appspot.com/?q="
+    connection = urllib.urlopen(url + text_to_check)
+    output = connection.read()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    connection.close()
 
 read_text()
