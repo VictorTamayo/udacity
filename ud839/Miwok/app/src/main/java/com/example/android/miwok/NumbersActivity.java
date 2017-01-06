3b05d5aed05f1c7748ce98778eefdff3ea073a3f
Victor Tamayo <victor.t.barriga@gmail.com>
2017-01-05 22:43:03 -0800

    add arraylist and textviews to numbersActivity

--- ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,14 +2,44 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    ArrayList<String> mWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        mWords = new ArrayList<String>();
+        mWords.add("One");
+        mWords.add("Two");
+        mWords.add("Three");
+        mWords.add("Four");
+        mWords.add("Five");
+        mWords.add("Six");
+        mWords.add("Seven");
+        mWords.add("Eight");
+        mWords.add("Nine");
+        mWords.add("Ten");
+
+        //find the root view for the whole layout
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        //add textviews with each element of the words array
+        for(String word : mWords) {
+            TextView r = new TextView(this);
+            r.setText(word);
+            rootView.addView(r);
+        }
+
     }
 
 
