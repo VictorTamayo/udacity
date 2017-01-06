cd7996425aaef3cc53226addaac8cff965adcebe
Victor Tamayo <victor.t.barriga@gmail.com>
2017-01-06 00:57:49 -0800

    added listview for the numbers activity

--- ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,15 +33,10 @@ public class NumbersActivity extends AppCompatActivity {
         mWords.add("Ten");
 
         //find the root view for the whole layout
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-        //add textviews with each element of the words array
-        for(String word : mWords) {
-            TextView r = new TextView(this);
-            r.setText(word);
-            rootView.addView(r);
-        }
-
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mWords);
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
     }
 
 
