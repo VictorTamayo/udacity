20916de4a388bf71050fe9b399f3ab1f0c58458d
Victor Tamayo <victor.t.barriga@gmail.com>
2017-01-11 06:02:33 -0800

    added icons to the project

--- ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,17 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-
-import static com.example.android.miwok.R.layout.list_item;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -21,19 +13,19 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("One","lutti"));
-        words.add(new Word("Two","otiiko"));
-        words.add(new Word("Three","tolookosu"));
-        words.add(new Word("Four","oyyisa"));
-        words.add(new Word("Five","massokka"));
-        words.add(new Word("Six","temmokka"));
-        words.add(new Word("Seven","kenekaku"));
-        words.add(new Word("Eight","kawinta"));
-        words.add(new Word("Nine","wo'e"));
-        words.add(new Word("Ten","na'aacha"));
+        words.add(new Word("One","lutti",R.drawable.number_one));
+        words.add(new Word("Two","otiiko",R.drawable.number_two));
+        words.add(new Word("Three","tolookosu",R.drawable.number_three));
+        words.add(new Word("Four","oyyisa",R.drawable.number_four));
+        words.add(new Word("Five","massokka",R.drawable.number_five));
+        words.add(new Word("Six","temmokka",R.drawable.number_six));
+        words.add(new Word("Seven","kenekaku",R.drawable.number_seven));
+        words.add(new Word("Eight","kawinta",R.drawable.number_eight));
+        words.add(new Word("Nine","wo'e",R.drawable.number_nine));
+        words.add(new Word("Ten","na'aacha",R.drawable.number_ten));
 
         //find the root view for the whole layout
 
--- ud839/Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ ud839/Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -5,6 +5,7 @@ package com.example.android.miwok;
  *  It contains a default translation and a Miwok translation for that word.
  */
 
+
 public class Word {
 
     // String Miwok word
@@ -13,12 +14,25 @@ public class Word {
     //  String English Word
     private String mDefaultTranslation;
 
+    // int containing img resource id
+    private int mImageResourceId;
+
     /***
      *  Constructs a new word with initial miwok and english translations.
      **/
     public Word(String miwok, String english){
         mMiwokTranslation = miwok;
         mDefaultTranslation = english;
+    }
+
+
+    /***
+     * Constructs a new word with initial miwok to english translation and an image
+     * */
+    public Word(String miwok, String english, int imageResourceId){
+        mMiwokTranslation = miwok;
+        mDefaultTranslation = english;
+        mImageResourceId = imageResourceId;
     }
 
     // Get the Miwok translation of the word.
@@ -29,5 +43,9 @@ public class Word {
     //Get the default translation of the word.
     public String getDefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
